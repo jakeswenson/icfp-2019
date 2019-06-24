6d3fa7fd344ecf22d538601ac639e74f038ea0be
teemo <teemobean@icloud.com>
2019-06-24 01:39:12 -0700

    Perf (#170)

--- src/main/kotlin/icfp2019/strategies/BFSStrategy.kt
+++ src/main/kotlin/icfp2019/strategies/BFSStrategy.kt
@@ -24,15 +24,14 @@ object BFSStrategy : Strategy {
             // } else if (gameState.boostersAvailable(Booster.FastWheels) > 0) {
             //    Action.AttachFastWheels
             } else {
-                val unWrappedPoints = gameState.boardState().allStates().filter { !it.isWrapped }.map { it.point }
                 val graph = graphBuilder.invoke(robotId, gameState)
                 val currentPoint = gameState.robot(robotId).currentPosition
-                val currentNode = graph.vertexSet().filter { currentPoint == it.point }[0]
+                val currentNode = gameState.get(currentPoint)
 
                 val unwrappedGraph =
                     AsSubgraph(
                         graph,
-                        graph.vertexSet().filter { it.point in unWrappedPoints }.plus(currentNode).toSet()
+                        graph.vertexSet().filter { gameState.nodeState(it.point).isWrapped.not() }.plus(currentNode).toSet()
                     )
 
                 val bfsIterator: GraphIterator<BoardCell, DefaultEdge> = BreadthFirstIterator(unwrappedGraph, currentNode)
@@ -41,7 +40,7 @@ object BFSStrategy : Strategy {
                     .filter { gameState.isInBoard(it) }
                     .map { gameState.get(it) }
                 if (neighbors.any {
-                        it.point in unWrappedPoints && it.isObstacle.not()
+                        gameState.nodeState(it.point).isWrapped.not() && it.isObstacle.not()
                     }) {
                     bfsIterator.next() // move past currentNode
                     val neighbor = bfsIterator.next().point
@@ -53,7 +52,7 @@ object BFSStrategy : Strategy {
                     val pathToClosestNode: GraphPath<BoardCell, DefaultEdge> = unwrappedGraph.vertexSet()
                         .asSequence()
                         .filter { it.point != currentNode.point }
-                        .filter { it.point in unWrappedPoints }
+                        .filter { gameState.nodeState(it.point).isWrapped.not() }
                         .sample(50)
                         .map { shortestPathAlgorithm.getPath(gameState.get(currentPoint), it) }
                         .minBy { it.length }!!
